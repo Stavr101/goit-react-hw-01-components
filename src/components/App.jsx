af54bb048def1765471af274333003d927687d02
Sergey Shishkin <Shishkin_SU@ukr.net>
2022-09-20 20:48:52 +0300

    bug fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,16 +5,17 @@ import data from 'data/data.json';
 import friends from 'data/friends.json';
 import transactions from 'data/transactions.json';
 
-import { Statistics } from './statistics/Statistic';
-import { FriendList } from './friendList/FriendList';
-import { TransactionHistory } from './transactionHistory/TransactionHistory';
-import { UserProfile } from './user/UserProfile';
+import { Statistics } from './Statistics/Statistic';
+import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import { UserProfile } from './User/UserProfile';
 
 export const App = () => {
   return (
     <Box>
       <UserProfile user={user} />
-      <Statistics data={data} />
+
+      <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
       <TransactionHistory items={transactions} />
     </Box>
--- src/components/statistics/Statistic.jsx
+++ src/components/statistics/Statistic.jsx
@@ -1,19 +1,19 @@
 import PropTypes from 'prop-types';
 
-// import data from 'data/data.json';
-import { TitleList } from './Title';
 import { StatList } from './Stats.jsx';
 import { Box } from 'components/Box';
+import { StatTitle } from './Statistic.styled';
 
-export const Statistics = ({ data }) => {
+export const Statistics = ({ title, stats }) => {
   return (
     <Box>
-      <TitleList title="Upload stats" />
-      <StatList data={data} />
+      {title && <StatTitle>{title}</StatTitle>}
+      <StatList data={stats} />
     </Box>
   );
 };
 
 Statistics.propTypes = {
-  data: PropTypes.array,
+  stats: PropTypes.array,
+  title: PropTypes.string,
 };
