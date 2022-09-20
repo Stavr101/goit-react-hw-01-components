79f3bf46aa1c9930db835e7392454141e9f6809a
Sergey Shishkin <95885361+Stavr101@users.noreply.github.com>
2022-09-20 15:36:24 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,9 @@
 import { Box } from './Box';
 
-import user from 'data/user.json';
-import data from 'data/data.json';
-import friends from 'data/friends.json';
-import transactions from 'data/transactions.json';
+import user from '../data/user.json';
+import data from '../data/data.json';
+import friends from '../data/friends.json';
+import transactions from '../data/transactions.json';
 
 import { Statistics } from './statistics/Statistic';
 import { FriendList } from './friendList/FriendList';
