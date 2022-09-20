c4ca30ebf49cb57bc4047de59bf427a4cdfc44b5
Sergey Shishkin <Shishkin_SU@ukr.net>
2022-09-20 16:07:50 +0300

    123

--- src/components/User/UserProfile.jsx
+++ src/components/User/UserProfile.jsx
@@ -1,6 +1,5 @@
 import PropTypes from 'prop-types';
 
-// import user from 'data/user.json';
 import { UserDescription } from './UserDescription/UserDescription';
 import { UserStats } from './Stats/UserStats';
 import { Profile } from './UserProfile.styled.jsx';
